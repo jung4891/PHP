b0cde5c4ca3c6f09ed865080a70d4c28587a33f8
HyukJung <go_go_ssing@naver.com>
2021-08-26 23:36:08 +0900

    a

---
+++ 2. 타입/array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $arr = array('a', 'b', 'c');
+    echo $arr[1].'<br>';
+    var_dump(count($arr));    // int(3)
+    // print($arr);           // 안됨
+    array_push($arr, 'd', 'e');
+    print('<br>');
+    print_r($arr);   // Array ( [0] => a [1] => b [2] => c [3] => d [4] => e )
+    echo '<br>';
+    var_dump($arr);
+    // array(5) { [0]=> string(1) "a" [1]=> string(1) "b" [2]=> string(1) "c
+    //            [3]=> string(1) "d" [4]=> string(1) "e" }
+
+    // 페이지 소스보기에서 더 깔끔하게 볼 수 있다.
+
+     ?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -7,11 +7,24 @@
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+
+      <?php
+      // scandir()은 디렉토리에 있는 파일명들을 담아 배열로 리턴한다.
+      $dir = './data';
+      $files = scandir($dir);
+      $i = 0;
+      while($i < count($files)) {
+        if($files[$i] != '.' && $files[$i] != '..' ) {
+          echo "<li><a href=\"index.php?id=$files[$i]\">$files[$i]</a></li>\n";
+        }
+        $i = $i + 1;
+      }
+      // "" 안에 $가 오면 그 담에 오는게 변수명으로 인식한다. ''는 안됨. ``도 안됨.
+      // \n은 HTML이 아닌 일반텍스트문서에서 줄바꿈. (페이지소스페이 )
+       ?>
     </ol>
- 
+    <!-- <li><a href=index.php?id=$files[3]>$files[3]</a></li> -->
+
     <h2>
       <?php
       if (isset($_GET['id'])) {
@@ -22,12 +35,14 @@
       ?>
     </h2>
     <?php
-    if (isset($_GET['id'])) {  
+    if (isset($_GET['id'])) {
       $file = file_get_contents('data/'.$_GET['id']);
       echo $file;             // data/id 값에 해당하는 파일의 내용
     } else {
-      echo 'WEB 메인페이지입니다.'; 
+      echo 'WEB 메인페이지입니다.';
     }
+
+
 
      ?>
   </body>
