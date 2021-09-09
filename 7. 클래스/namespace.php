205a144a7d5af6cba5bf43567274cd7b5a3c2a6d
HyukJung <go_go_ssing@naver.com>
2021-09-10 06:28:17 +0900

    a

--- 7. 클래스/namespace.php
+++ 7. 클래스/namespace.php
@@ -6,15 +6,21 @@
   //  - 폴더 개념이며 다른 코드를 가져와 사용시 이름이 겹치지 않게 할 수 있다.
   //  - 골때리게 위에 <?php 이 코드 위에 어떤것도 오면 안된다. 엔터도 안되고 html 형식도 안된다.
   namespace agroup;
-  function printTest() {
-    return '첫번째 함수';
+  class a{
+    function printTest() {
+      return '첫번째 클래스의 메소드';
+    }
   }
-  namespace b;
-  function printTest() {
-    echo '두번째 함수';
-  }
+  namespace bgroup;
+    class a{
+      function printTest() {
+        return '두번째 클래스의 메소드';
+      }
+    }
 
-  echo \agroup\printTest();
+  $a = new \agroup\a;   // 객체생성
+  echo $a->printTest();
   echo '<br>';
-  \b\printTest();
+  $a2 = new \bgroup\a;
+  echo $a2->printTest();
 ?>
