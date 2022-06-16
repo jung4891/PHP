d5ad012ad59a42231e63a7f3a3eb29981c4df331
SongHyukJung <go_go_ssing@naver.com>
2022-06-16 18:06:13 +0900

    zzzz

--- zz/node/test/main.js
+++ zz/node/test/main.js
@@ -4,42 +4,50 @@ var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = "Welcome";
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
+    let parse_url = url.parse(_url, true);
+    var pathName = parse_url.pathname;
+    var queryStr = parse_url.query;
+    var title = queryStr.id;
 
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
+    if(pathName === '/'){
+      fs.readFile(`data/${title}`, 'utf8', function(err, content){
+
+        if(title === undefined) {
+            title = 'Welcome!';
+            content = "환영합니다!";
+        }
+
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
         <li><a href="/?id=HTML">HTML</a></li>
         <li><a href="/?id=CSS">CSS</a></li>
         <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
-    `
-    response.end(template);
-    // response.end(fs.readFileSync(__dirname + _url));
-    // response.end(`id: ${queryData.id} / page: ${queryData.page}`);
+        </ul>
+        <h2>${title}</h2>
+        <p>${content}</p>
+        </body>
+        </html>
+        `
+        response.writeHead(200);
+        response.end(template);
+        // response.end(fs.readFileSync(__dirname + _url));
+        // response.end(`id: ${queryData.id} / page: ${queryData.page}`);
+      });
+    }else {
+      response.writeHead(404);
+      response.end('Not Found');
+    }
+
+
+
+
 });
 app.listen(4000);
