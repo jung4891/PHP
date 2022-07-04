0a1fa75204e4d46b4b4640abd0c2aa32ed093818
SongHyukJung <go_go_ssing@naver.com>
2022-07-04 18:07:31 +0900

    zzzz

--- zz/node/chat-test3/index.js
+++ zz/node/chat-test3/index.js
@@ -1,11 +1,25 @@
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require("socket.io")(server);
 
-const { Server } = require("socket.io");
-const io = new Server(server);
+let port = 3000;
+server.listen(port, () => {
+  console.log(`listening on *: ${port} ~!!`);
+});
+
+let mysql = require('mysql');
+let conn = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'chat_test'
+})
+
+conn.query('SELECT * FROM chat_log', function(err, rows, fields) {
+
+})
 
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello world</h1>');
@@ -16,20 +30,16 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected ~~!! 123');
-  // console.log('socket: ', socket);
+// socket.emit('msg_test', `${socket.id} 연결 되었습니다.`);         // 나에게만 전송됨
+  io.emit('msg_from_server', `${socket.id}님이 입장하였습니다~!`);          // 모두에게 전송됨
+  console.log(`${socket.id} : 입장`);
+
+  socket.on('msg_from_client', (msg) => {
+    io.emit('msg_from_server', `${socket.id}: ${msg}`);
+  });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected....');
+    io.emit('msg_from_server', `${socket.id}님이 방을 나갔습니다.`);
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message_test', msg);
-    console.log('메시지: ' + msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-  // console.log(io);
 });
