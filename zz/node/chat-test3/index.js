129312063edd89f1060cde33e3a5b6595bea1e2d
SongHyukJung <go_go_ssing@naver.com>
2022-07-06 18:00:28 +0900

    zzz

--- zz/node/chat-test3/index.js
+++ zz/node/chat-test3/index.js
@@ -1,7 +1,7 @@
 
 const app = require('express')();
 const server = require('http').createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 let port = 3000;
 server.listen(port, () => {
